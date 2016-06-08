712a3c0d5bb7e5204eb7cf766251906f8f65d6c9
jaswant99k <jaswantkumar999@gmail.com>
2016-06-08 17:28:48 +0530

    PMS
    Project Management system

---
+++ PMS/PMS/ADMIN/report.php
@@ -0,0 +1,113 @@
+<?php
+session_start();
+$user =  $_SESSION['Email'];
+$role = $_SESSION['Role'];
+if(empty($_SESSION['Email']))
+{
+header("location:index.php");
+}
+else
+{
+if($role=="Admin")
+{
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style>
+	body
+	{
+		background-image:url(../background.png);
+		background-repeat: no-repeat; 
+		background-attachment: fixed;
+		background-size: 100% 100%;
+	}
+</style>
+<title>Project Management System</title>
+</head>
+<div>
+<body>
+    <table width="100%"  border="0"cellspacing="00" cellpadding="00">
+  <tr bgcolor="#D2691E">
+    <th width="74" scope="col">&nbsp;</th>
+    <th width="164" scope="col"><a href="../Admin.php"><img src="../logo1.png" alt="LOGO"/></a></th>
+    <th width="646" scope="col"><font size="8" color="White">Project Managenent System</font></th>
+    <th width="140" scope="col"><font color="White" size="5">
+	<?php
+    print $role;
+    ?></font></th>
+    <th width="63" scope="col">&nbsp;</th>
+  </tr>
+</table>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr bgcolor="#99CCFF">
+      <th width="5%" scope="col"><h4>&nbsp;</h4></th>
+      <th width="12%" scope="col"><a href="student.php">Add Student</a></th>
+      <th width="11%" scope="col"><a href="faculty.php">Add Faculty</a></th>
+      <th width="11%" scope="col"><a href="stsearch.php">Search Student</a></th>
+      <th width="11%" scope="col"><a href="fa_search.php">Search Faculty </a></th>
+      <th width="11%" scope="col"><a href="allocate.php">Allocate</a></th>
+      <th width="11%" scope="col"><a href="skill.php">Skill Matrix</a></th>
+      <th width="11%" scope="col"><a href="report.php">Reports</a></th>
+      <th width="11%" scope="col"><a href="../logout.php">Logout</a></th>
+    <th width="6%" scope="col">&nbsp;</th>
+  </tr>
+</table>
+    <br/><br/><br/>
+    <table  border="1" align="center" bgcolor="red">
+            <?php
+                echo "<tr>";
+                echo "<th>"."Meeting ID"."</th>";
+                echo "<th>" ?> &nbsp; <?php "</th>";
+                echo "<th>"."Faculty ID"."</th>";
+                echo "<th>" ?> &nbsp; <?php "</th>";
+                echo "<th>"."Student ID"."</th>";
+                echo "<th>" ?> &nbsp; <?php "</th>";
+                echo "<th>"."Date"."</th>";
+                echo "<th>" ?> &nbsp; <?php "</th>";
+                echo "<th>"."Time"."</th>";
+                echo "<th>" ?> &nbsp; <?php "</th>";
+                echo "<th>"."Description"."</th>";
+                echo "</tr>";
+                include './connection.php';
+                        $sql1="select * from meeting ";
+                        $rec=mysqli_query($conn, $sql1);
+                        while ($std=mysqli_fetch_assoc($rec))
+                        {
+                           ?> <tr bgcolor="beige" align="center"><?php
+                            echo "<td>".$std['meeting_id']."<td/>";
+                            echo "<td>".$std['f_id']."<td/>"; 
+                            echo "<td>".$std['s_id']."<td/>"; 
+                            echo "<td>".$std['date']."<td/>"; 
+                            echo "<td>".$std['time']."<td/>"; 
+                            echo "<td>".$std['desc']."<td/>"; 
+                            ?>  </tr> <?php 
+                        }
+            ?>
+        </table>
+    </form>
+ <?php
+ 
+}
+elseif($role=="Faculty")    
+{
+    header('Location:../Admin.php'); 
+?>
+ <?php
+}
+else   
+{
+    header('Location:../Admin.php'); 
+?>
+      
+<?php
+}
+?>
+</table>
+<p>
+  <?php
+}
+?>
+   
